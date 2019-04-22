e27e97dbcecc2ed917e10c851c095c350afe8066
wanghao <wanghao@titgroup.cn>
2019-04-22 16:14:39 +0800

    init

---
+++ src/main/java/com/haohao/labmanagespringboot/LabManageSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.haohao.labmanagespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabManageSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabManageSpringbootApplication.class, args);
+    }
+
+}
